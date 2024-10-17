a756efee11fe3f93940319e5abf0aa6f1abb3531
jemyJR <mmohamedabdelrahman@student.eelu.edu.eg>
2024-10-17 20:08:44 +0300

    -

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:codeness_lab_task/codeness_lab_task.dart';
 import 'package:codeness_lab_task/features/task/data/models/task_model.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
-
 import 'core/routing/app_router.dart';
 
 void main() async {
